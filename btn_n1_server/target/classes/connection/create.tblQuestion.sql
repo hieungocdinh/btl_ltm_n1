263ba723d651fe5e2cacafddb971ab3a1fb8b969
sieupkcool <vietanh10120@gmail.com>
2024-10-12 10:03:32 +0700

    add matchView

---
+++ btn_n1_server/target/classes/connection/create.tblQuestion.sql
@@ -0,0 +1,16 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  hieun
+ * Created: Oct 5, 2024
+ */
+CREATE TABLE `btlltm`.`questions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `questionText` TEXT NOT NULL,
+  `imageLink` TEXT NOT NULL,
+  `answer` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
---
+++ btn_n1_server/target/classes/connection/create.tblUser.sql
@@ -0,0 +1,17 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  hieun
+ * Created: Oct 5, 2024
+ */
+
+CREATE TABLE `btlltm`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(50) NOT NULL,
+  `full_name` VARCHAR(50) NOT NULL,
+  `total_score` INT NOT NULL,
+  PRIMARY KEY (`id`)
+);
